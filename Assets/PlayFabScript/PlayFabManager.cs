0d814918682873120b61e0118af2797a52932597
hondashion <s.honda.crypto@gmail.com>
2024-07-21 14:25:58 +0900

    ログイン処理修正

--- Assets/PlayFabScript/PlayFabManager.cs
+++ Assets/PlayFabScript/PlayFabManager.cs
@@ -127,6 +127,7 @@ public class PlayFabManager : MonoBehaviour
         var bots = BotManager.Instance.GetBots();
         foreach (var bot in bots)
         {
+            // 元の1時間単位に戻す
             int botCoins = bot.GetCurrentScorePerHour() * (int)offlineDuration.TotalHours;
             totalCoinsToAdd += botCoins;
             Debug.Log($"Bot: {bot.Name}, Level: {bot.Level}, Coins Added: {botCoins}");
@@ -139,6 +140,7 @@ public class PlayFabManager : MonoBehaviour
     private void Login()
     {
         _customID = LoadCustomID();
+        _shouldCreateAccount = true; // 強制的に新しいアカウントを作成するよう設定
         Debug.Log("CustomID: " + _customID);
         var request = new LoginWithCustomIDRequest { CustomId = _customID, CreateAccount = _shouldCreateAccount };
         PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
@@ -178,12 +180,6 @@ public class PlayFabManager : MonoBehaviour
         if (_shouldCreateAccount)
         {
             id = GenerateCustomID();
-            _customID = id;
-            SaveCustomID();
-        }
-        else
-        {
-            _customID = id;
         }
 
         return id;
