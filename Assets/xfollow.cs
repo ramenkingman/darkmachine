15773ccb10b50952818538039d371dbebf218555
issihiki <127279333+issihiki@users.noreply.github.com>
2024-07-30 13:30:16 +0900

    タスク修正

--- Assets/xfollow.cs
+++ Assets/xfollow.cs
@@ -1,53 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
 
-public class XFollowButton : MonoBehaviour
+public class ButtonHandler : MonoBehaviour
 {
-    public Button xFollowButton; // ボタンをインスペクターで指定
+    public Button button1;
+    public Button button2;
+    public GameObject warningObject; // 特定のオブジェクトをインスペクターで指定
+
+    private bool hasPressedButton1 = false;
+    private bool hasIncreasedScore = false;
 
     private void Start()
     {
-        if (xFollowButton != null)
+        if (button1 != null)
         {
-            xFollowButton.onClick.AddListener(OnXFollowButtonClicked);
+            button1.onClick.AddListener(OnButton1Clicked);
         }
+
+        if (button2 != null)
+        {
+            button2.onClick.AddListener(OnButton2Clicked);
+        }
+
+        // PlayFabから初期状態を取得
+        LoadPlayerData();
     }
 
-    private void OnXFollowButtonClicked()
+    private void OnButton1Clicked()
     {
-        if (PlayFabManager.Instance == null)
+        hasPressedButton1 = true;
+        SaveButton1Pressed();
+    }
+
+    private void OnButton2Clicked()
+    {
+        if (hasPressedButton1)
         {
-            Debug.LogError("PlayFabManagerのインスタンスが見つかりません。");
-            return;
-        }
-
-        if (!PlayFabManager.Instance.IsDataLoaded())
-        {
-            Debug.LogWarning("データがロードされていないため、処理を実行できません。");
-            return;
-        }
-
-        var xFollow = PlayFabManager.Instance.XFollowToSave;
-        if (xFollow > 0)
-        {
-            // xFollowの値を100増加させる
-            PlayFabManager.Instance.IncreaseXFollow(100);
-            xFollow = PlayFabManager.Instance.XFollowToSave; // 更新後の値を取得
-
-            var currentScore = ScoreManager.Instance.Score + 5000;
-            ScoreManager.Instance.SetScore(currentScore);
-
-            var currentLevel = LevelManager.Instance.PlayerLevel;
-            var botLevels = BotManager.Instance.GetBotLevels();
-            var invitation = PlayFabManager.Instance.InvitationToSave;
-            var currentEnergy = EnergyManager.Instance.CurrentEnergy; // 追加
-
-            // データをPlayFabに保存
-            PlayFabManager.Instance.SavePlayerData(currentScore, currentLevel, xFollow, invitation, botLevels, currentEnergy); // 追加
+            if (!hasIncreasedScore)
+            {
+                var currentScore = ScoreManager.Instance.Score + 5000;
+                ScoreManager.Instance.SetScore(currentScore);
+                hasIncreasedScore = true;
+                SavePlayerData(currentScore);
+                button2.interactable = false; // ボタン2を非アクティブにする
+            }
+            else
+            {
+                button2.interactable = false; // 既にスコアが増加されている場合、ボタン2を常に非アクティブにする
+            }
         }
         else
         {
-            Debug.Log("XFollowの値が0のため、処理を実行しません。");
+            StartCoroutine(ShowWarning());
         }
+    }
+
+    private IEnumerator ShowWarning()
+    {
+        warningObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        warningObject.SetActive(false);
+    }
+
+    private void LoadPlayerData()
+    {
+        var request = new GetUserDataRequest
+        {
+            Keys = new List<string> { "HasPressedButton1", "HasIncreasedScore" }
+        };
+
+        PlayFabClientAPI.GetUserData(request, OnDataReceived, OnError);
+    }
+
+    private void OnDataReceived(GetUserDataResult result)
+    {
+        if (result.Data != null)
+        {
+            if (result.Data.ContainsKey("HasPressedButton1"))
+            {
+                hasPressedButton1 = bool.Parse(result.Data["HasPressedButton1"].Value);
+            }
+
+            if (result.Data.ContainsKey("HasIncreasedScore"))
+            {
+                hasIncreasedScore = bool.Parse(result.Data["HasIncreasedScore"].Value);
+            }
+
+            // 取得したデータに基づいてボタン2の状態を設定
+            button2.interactable = !hasIncreasedScore;
+        }
+        else
+        {
+            // データが存在しない場合もボタン2を非アクティブにする
+            button2.interactable = true;
+        }
+    }
+
+    private void SaveButton1Pressed()
+    {
+        var request = new UpdateUserDataRequest
+        {
+            Data = new Dictionary<string, string>
+            {
+                { "HasPressedButton1", hasPressedButton1.ToString() }
+            }
+        };
+
+        PlayFabClientAPI.UpdateUserData(request, OnDataSend, OnError);
+    }
+
+    private void SavePlayerData(int score)
+    {
+        var request = new UpdateUserDataRequest
+        {
+            Data = new Dictionary<string, string>
+            {
+                { "Score", score.ToString() },
+                { "HasIncreasedScore", hasIncreasedScore.ToString() }
+            }
+        };
+
+        PlayFabClientAPI.UpdateUserData(request, OnDataSend, OnError);
+    }
+
+    private void OnDataSend(UpdateUserDataResult result)
+    {
+        Debug.Log("プレイヤーデータが正常に保存されました！");
+    }
+
+    private void OnError(PlayFabError error)
+    {
+        Debug.LogError("プレイヤーデータの保存中にエラーが発生しました: " + error.GenerateErrorReport());
     }
 }
